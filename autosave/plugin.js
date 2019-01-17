a380db545f2880a335f07f8aae8054b740aaf91a
Geoff Horsey <geoff.horsey@optimityadvisors.com>
2019-01-16 16:49:11 -0800

    fix: loadPlugin is fired before extensions.min.js is loaded when using requirejs

--- autosave/plugin.js
+++ autosave/plugin.js
@@ -62,8 +62,9 @@
 
             editor.on('instanceReady', function () {
                 if (typeof requirejs === "function") {
-                    requirejs([CKEDITOR.getUrl(CKEDITOR.plugins.getPath('autosave') + 'js/extensions.min.js')]);
-                    loadPlugin(editor, config);
+                    requirejs([CKEDITOR.getUrl(CKEDITOR.plugins.getPath('autosave') + 'js/extensions.min.js')], function() {
+                        loadPlugin(editor, config);
+                    });
                 } else {
                     CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('autosave') + 'js/extensions.min.js'), function () {
                         loadPlugin(editor, config);
