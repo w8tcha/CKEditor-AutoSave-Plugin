474781d34437300c9a342402a134c51b44eb9cc6
w8tcha <thewatcher@watchersnet.de>
2019-10-13 05:13:09 +0200

    Changed quotes

--- autosave/plugin.js
+++ autosave/plugin.js
@@ -10,22 +10,22 @@
     }
 
     CKEDITOR.plugins.add("autosave", {
-        lang: 'bg,ca,cs,de,en,es,eu,fr,it,ja,nl,pl,pt-br,ru,sk,sv,zh,zh-cn', // %REMOVE_LINE_CORE%
-        requires: 'notification',
+        lang: "bg,ca,cs,de,en,es,eu,fr,it,ja,nl,pl,pt-br,ru,sk,sv,zh,zh-cn", // %REMOVE_LINE_CORE%
+        requires: "notification",
         version: "0.18.4",
         init: function (editor) {
 
             // Look for autosave from config.js - this is a bit redundant but necessary
-            editor.config.autosave = 'autosave' in editor.config ? editor.config.autosave : {};
+            editor.config.autosave = "autosave" in editor.config ? editor.config.autosave : {};
             
             // Prepare temp vars for constructing local storage SaveKey name
-            var _saveKeyPrefix = 'saveKeyPrefix' in editor.config.autosave ? editor.config.autosave.saveKeyPrefix : 'autosave',
-                _saveKeyIgnoreProto = 'saveKeyIgnoreProtocol' in editor.config.autosave ? editor.config.autosave.saveKeyIgnoreProtocol : false,
-                _saveKeyUrl = _saveKeyIgnoreProto ? window.location.href.replace(/https?:\/\//, '') : window.location.href,
-                _saveKeyDelimiter = 'saveKeyDelimiter' in editor.config.autosave ? editor.config.autosave.saveKeyDelimiter : '_',
-                _saveKeyAttribute = 'saveKeyAttribute' in editor.config.autosave ? editor.config.autosave.saveKeyAttribute : 'name';
+            var _saveKeyPrefix = "saveKeyPrefix" in editor.config.autosave ? editor.config.autosave.saveKeyPrefix : "autosave",
+                _saveKeyIgnoreProto = "saveKeyIgnoreProtocol" in editor.config.autosave ? editor.config.autosave.saveKeyIgnoreProtocol : false,
+                _saveKeyUrl = _saveKeyIgnoreProto ? window.location.href.replace(/https?:\/\//, "") : window.location.href,
+                _saveKeyDelimiter = "saveKeyDelimiter" in editor.config.autosave ? editor.config.autosave.saveKeyDelimiter : "_",
+                _saveKeyAttribute = "saveKeyAttribute" in editor.config.autosave ? editor.config.autosave.saveKeyAttribute : "name";
 
-            if ('saveKeyIgnoreParams' in editor.config.autosave) {
+            if ("saveKeyIgnoreParams" in editor.config.autosave) {
                 CKEDITOR.tools.array.forEach(editor.config.autosave.saveKeyIgnoreParams,
                     function() {
                         _saveKeyUrl = autosaveRemoveUrlParam(this, null, _saveKeyUrl);
@@ -47,32 +47,32 @@
             // Extend CKEDITOR config and lang  - config also available at loadPlugin()
             var config = CKEDITOR.tools.extend(defaultConfig, editor.config.autosave || {}, true);
 
-            CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('autosave') + 'css/autosave.min.css'));
+            CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(CKEDITOR.plugins.getPath("autosave") + "css/autosave.min.css"));
 
-            editor.addCommand( 'removeAutoSaveStorage', {
+            editor.addCommand( "removeAutoSaveStorage", {
                 exec: function( editor ) {
                     RemoveStorage(config.SaveKey, editor);
                 }
             });
             
-            editor.on('uiSpace', function(event) {
-                if (event.data.space == 'bottom' && config.messageType != null && config.messageType == "statusbar") {
+            editor.on("uiSpace", function(event) {
+                if (event.data.space == "bottom" && config.messageType != null && config.messageType == "statusbar") {
 
                     event.data.html += '<div class="autoSaveMessage" unselectable="on"><div unselectable="on" id="'
                         + autoSaveMessageId(event.editor)
                         + '"class="hidden">'
                         + event.editor.lang.autosave.autoSaveMessage
-                        + '</div></div>';
+                        + "</div></div>";
                 }
             }, editor, null, 100);
 
-            editor.on('instanceReady', function () {
+            editor.on("instanceReady", function () {
                 if (typeof requirejs === "function") {
-                    requirejs([CKEDITOR.getUrl(CKEDITOR.plugins.getPath('autosave') + 'js/extensions.min.js')], function() {
+                    requirejs([CKEDITOR.getUrl(CKEDITOR.plugins.getPath("autosave") + "js/extensions.min.js")], function() {
                         loadPlugin(editor, config);
                     });
                 } else {
-                    CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('autosave') + 'js/extensions.min.js'), function () {
+                    CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath("autosave") + "js/extensions.min.js"), function () {
                         loadPlugin(editor, config);
                     });
                 }
@@ -87,16 +87,16 @@
         CheckForAutoSavedContent(editorInstance, config, config.SaveKey, config.NotOlderThen);
 
         CKEDITOR.tools.array.forEach(CKEDITOR.document.find(config.saveDetectionSelectors).toArray(), function(el) {
-            el.$.addEventListener('click', function () {
+            el.$.addEventListener("click", function () {
                 RemoveStorage(config.SaveKey, editorInstance);
             });
         });
 
-        editorInstance.on('change', function() {
+        editorInstance.on("change", function() {
             startTimer(config, editorInstance);
         });
 
-        editorInstance.on('destroy', function() {
+        editorInstance.on("destroy", function() {
             if (config.saveOnDestroy) {
                 SaveData(config.SaveKey, editorInstance, config);
             }
@@ -104,7 +104,7 @@
     }
 
     function autoSaveMessageId(editorInstance) {
-        return 'cke_autoSaveMessage_' + editorInstance.name;
+        return "cke_autoSaveMessage_" + editorInstance.name;
     }
 
     var startTimer = function (configAutosave, editorInstance) {
@@ -121,7 +121,7 @@
             var editor = editorInstance,
                 autoSaveKey = configAutosave.SaveKey != null
                     ? configAutosave.SaveKey
-                    : 'autosave_' + window.location + "_" + document.getElementById(editor.name).getAttribute('name');
+                    : "autosave_" + window.location + "_" + document.getElementById(editor.name).getAttribute("name");
 
             SaveData(autoSaveKey, editor, configAutosave);
 
@@ -133,7 +133,7 @@
 
     // localStorage detection
     function supportsLocalStorage() {
-        if (typeof (Storage) === 'undefined') {
+        if (typeof (Storage) === "undefined") {
             return false;
         }
 
@@ -146,7 +146,7 @@
     }
 
     function GenerateAutoSaveDialog(editorInstance, config, autoSaveKey) {
-        CKEDITOR.dialog.add('autosaveDialog', function() {
+        CKEDITOR.dialog.add("autosaveDialog", function() {
             return {
                 title: editorInstance.lang.autosave.title,
                 minHeight: 155,
@@ -169,46 +169,46 @@
                 },
                 contents: [
                     {
-                        label: '',
-                        id: 'general',
+                        label: "",
+                        id: "general",
                         elements: [
                             {
-                                type: 'radio',
-                                id: 'diffType',
+                                type: "radio",
+                                id: "diffType",
                                 label: editorInstance.lang.autosave.diffType,
-                                items: [[editorInstance.lang.autosave.sideBySide, 'sideBySide'], [editorInstance.lang.autosave.inline, 'inline']],
+                                items: [[editorInstance.lang.autosave.sideBySide, "sideBySide"], [editorInstance.lang.autosave.inline, "inline"]],
                                 'default': config.diffType,
                                 onClick: function() {
                                     RenderDiff(this._.dialog, editorInstance, autoSaveKey);
                                 }
                             }, {
-                                type: 'html',
-                                id: 'diffContent',
-                                html: ''
+                                type: "html",
+                                id: "diffContent",
+                                html: ""
                             }
                         ]
                     }
                 ],
                 buttons: [
                     {
-                        id: 'ok',
-                        type: 'button',
+                        id: "ok",
+                        type: "button",
                         label: editorInstance.lang.autosave.ok,
-                        'class': 'cke_dialog_ui_button_ok cke_dialog_autosave_ok',
+                        'class': "cke_dialog_ui_button_ok cke_dialog_autosave_ok",
                         onClick: function(evt) {
                             var dialog = evt.data.dialog;
-                            if (dialog.fire('ok', { hide: true }).hide !== false)
+                            if (dialog.fire("ok", { hide: true }).hide !== false)
                                 dialog.hide();
                         }
                     },
                     {
-                        id: 'cancel',
-                        type: 'button',
+                        id: "cancel",
+                        type: "button",
                         label: editorInstance.lang.autosave.no,
-                        'class': 'cke_dialog_ui_button_cancel',
+                        'class': "cke_dialog_ui_button_cancel",
                         onClick: function(evt) {
                             var dialog = evt.data.dialog;
-                            if (dialog.fire('cancel', { hide: true }).hide !== false)
+                            if (dialog.fire("cancel", { hide: true }).hide !== false)
                                 dialog.hide();
                         }
                     }
@@ -234,7 +234,7 @@
             }
 
             // Ignore if autosaved content is older then x minutes
-            if (moment(new Date()).diff(new Date(autoSavedContentDate), 'minutes') > notOlderThen) {
+            if (moment(new Date()).diff(new Date(autoSavedContentDate), "minutes") > notOlderThen) {
                 RemoveStorage(autoSaveKey, editorInstance);
 
                 return;
@@ -254,7 +254,7 @@
 
                 if (confirm(confirmMessage)) {
                     // Open DIFF Dialog
-                    editorInstance.openDialog('autosaveDialog');
+                    editorInstance.openDialog("autosaveDialog");
                 } else {
                     RemoveStorage(autoSaveKey, editorInstance);
                 }
@@ -282,7 +282,7 @@
         }
 
         if (quotaExceeded) {
-            var notificationError = new CKEDITOR.plugins.notification(editorInstance, { message: editorInstance.lang.autosave.localStorageFull, type: 'warning' });
+            var notificationError = new CKEDITOR.plugins.notification(editorInstance, { message: editorInstance.lang.autosave.localStorageFull, type: "warning" });
             notificationError.show();
         } else {
             var messageType = config.messageType != null ? config.messageType : "notification";
@@ -302,7 +302,7 @@
                     }, 2000);
                 }
             } else if (messageType == "notification") {
-                var notification = new CKEDITOR.plugins.notification(editorInstance, { message: editorInstance.lang.autosave.autoSaveMessage, type: 'success' });
+                var notification = new CKEDITOR.plugins.notification(editorInstance, { message: editorInstance.lang.autosave.autoSaveMessage, type: "success" });
                 notification.show();
             }
         }
@@ -324,15 +324,15 @@
         var sm = new difflib.SequenceMatcher(base, newtxt);
         var opcodes = sm.get_opcodes();
 
-        dialog.getContentElement('general', 'diffContent').getElement().setHtml('<div class="diffContent">' + diffview.buildView({
+        dialog.getContentElement("general", "diffContent").getElement().setHtml('<div class="diffContent">' + diffview.buildView({
             baseTextLines: base,
             newTextLines: newtxt,
             opcodes: opcodes,
             baseTextName: editorInstance.lang.autosave.loadedContent,
-            newTextName: editorInstance.lang.autosave.autoSavedContent + (moment(jsonSavedContent.saveTime).locale(editorInstance.config.language).format(editorInstance.lang.autosave.dateFormat)) + '\'',
+            newTextName: editorInstance.lang.autosave.autoSavedContent + (moment(jsonSavedContent.saveTime).locale(editorInstance.config.language).format(editorInstance.lang.autosave.dateFormat)) + "'",
             contextSize: 3,
-            viewType: dialog.getContentElement('general', 'diffType').getValue() == "inline" ? 1 : 0
-        }).outerHTML + '</div>');
+            viewType: dialog.getContentElement("general", "diffType").getValue() == "inline" ? 1 : 0
+        }).outerHTML + "</div>");
     }
 
     function isQuotaExceeded(e) {
@@ -345,7 +345,7 @@
                         break;
                     case 1014:
                         // Firefox
-                        if (e.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
+                        if (e.name === "NS_ERROR_DOM_QUOTA_REACHED") {
                             quotaExceeded = true;
                         }
                         break;
@@ -366,23 +366,23 @@
             hash;
 
         if (re.test(url)) {
-            if (typeof value !== 'undefined' && value !== null) {
-                return url.replace(re, '$1' + key + "=" + value + '$2$3');
+            if (typeof value !== "undefined" && value !== null) {
+                return url.replace(re, "$1" + key + "=" + value + "$2$3");
             } else {
-                hash = url.split('#');
-                url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
-                if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
-                    url += '#' + hash[1];
+                hash = url.split("#");
+                url = hash[0].replace(re, "$1$3").replace(/(&|\?)$/, "");
+                if (typeof hash[1] !== "undefined" && hash[1] !== null) {
+                    url += "#" + hash[1];
                 }
                 return url;
             }
         } else {
-            if (typeof value !== 'undefined' && value !== null) {
-                var separator = url.indexOf('?') !== -1 ? '&' : '?';
-                hash = url.split('#');
-                url = hash[0] + separator + key + '=' + value;
-                if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
-                    url += '#' + hash[1];
+            if (typeof value !== "undefined" && value !== null) {
+                var separator = url.indexOf("?") !== -1 ? "&" : "?";
+                hash = url.split("#");
+                url = hash[0] + separator + key + "=" + value;
+                if (typeof hash[1] !== "undefined" && hash[1] !== null) {
+                    url += "#" + hash[1];
                 }
                 return url;
             } else {
