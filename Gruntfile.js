ae9facbd10cd746c855821d75bac3d5a044838bb
Ingo <thewatcher@watchersnet.de>
2023-07-26 08:53:51 +0200

    switched grunt-dev-update module

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-dev-update");
+  grunt.loadNpmTasks("@w8tcha/grunt-dev-update");
 
 
   grunt.registerTask("watch",
